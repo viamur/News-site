284dbb2a9be9760d1a9a0ebe39bec819089985a9
Moskalenko Serhii <mynameserhii@gmail.com>
2022-11-16 19:06:36 +0200

    footer Start

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { footerMenu, footerNewsCompanyMenu, socList } from '../../utils/utils';
+import { useDeskScreen } from '../../utils/useMediaQuery';
 import Container from '../Container/Container';
 
 // @mui
@@ -12,49 +13,66 @@ import icon from '../../images/icon/accordion.svg';
 import s from './Footer.module.scss';
 
 const Footer = () => {
+  const isDesk = useDeskScreen();
+
   return (
     <footer className={s.footer}>
       <Container>
-        <ul className={s.list}>
-          {footerMenu.map(el => {
-            return (
-              <li key={el.id} className={s.item}>
-                {el.submenu ? (
-                  <Accordion className={s.accordion}>
-                    <AccordionSummary
-                      className={s.accordion__summary}
-                      expandIcon={<img src={icon} alt={'icon'} height={16} width={16} />}
-                    >
-                      {el.title}
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <ul className={s.submenu}>
-                        {el.submenu.map(item => {
-                          return (
-                            <li key={item.id} className={s.submenu__item}>
-                              <Link className={s.submenu__link}>{item.title} </Link>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </AccordionDetails>
-                  </Accordion>
-                ) : (
-                  <Link className={s.item__title}>{el.title}</Link>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-        <ul className={s.secondMenu}>
-          {footerNewsCompanyMenu.map(el => {
-            return (
-              <li key={el.id} className={s.secondMenu__item}>
-                <Link className={s.secondMenu__link}>{el.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={s.wrapTop}>
+          <ul className={s.list}>
+            {footerMenu.map(el => {
+              return (
+                <li key={el.id} className={s.item}>
+                  {el.submenu && !isDesk ? (
+                    <Accordion className={s.accordion}>
+                      <AccordionSummary
+                        className={s.accordion__summary}
+                        expandIcon={<img src={icon} alt={'icon'} height={16} width={16} />}
+                      >
+                        {el.title}
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <ul className={s.submenu}>
+                          {el.submenu.map(item => {
+                            return (
+                              <li key={item.id} className={s.submenu__item}>
+                                <Link className={s.submenu__link}>{item.title} </Link>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </AccordionDetails>
+                    </Accordion>
+                  ) : (
+                    <>
+                      <Link className={s.item__title}>{el.title}</Link>
+                      {el.submenu && isDesk && (
+                        <ul className={s.submenu}>
+                          {el.submenu.map(item => {
+                            return (
+                              <li key={item.id} className={s.submenu__item}>
+                                <Link className={s.submenu__link}>{item.title} </Link>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      )}
+                    </>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+          <ul className={s.secondMenu}>
+            {footerNewsCompanyMenu.map(el => {
+              return (
+                <li key={el.id} className={s.secondMenu__item}>
+                  <Link className={s.secondMenu__link}>{el.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <div className={s.soc}>
           <h4 className={s.soc__title}>Новини у зручному форматі</h4>
           <ul className={s.soc__list}>
